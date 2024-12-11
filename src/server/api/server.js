6af07c1069fac0462a99100caa6b087c5b1a7de3
marailith <clairezheng.kasu@gmail.com>
2024-12-10 22:53:25 -0600

    cors issue

--- src/server/api/server.js
+++ src/server/api/server.js
@@ -12,14 +12,13 @@ dotenv.config();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://your-frontend-url.com"); // Replace with your frontend's URL
+	res.header("Access-Control-Allow-Origin", "https://nat-go.vercel.app");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	res.header("Access-Control-Allow-Credentials", "true");
 	
-	// Handle OPTIONS preflight request
 	if (req.method === "OPTIONS") {
-	  return res.sendStatus(204); // No Content
+	  return res.sendStatus(204);
 	}
   
 	next();
